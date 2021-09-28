a5c43e97b40e54e74eec129d8505395dccf766c8
Leo <nunochev@live.com>
2021-09-28 22:30:03 +0200

    Simulador JQ

--- application/include/simulador_data.php
+++ application/include/simulador_data.php
@@ -1,62 +1,102 @@
-<div class ="box">
+<div class ="box"  >
+
 
     <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Tipo de Credito</label>
-            <div class="col-sm-10">
-               <select name="credittype" class="form-control" required>
-                   <option value="CARC">CARC</option>
-                   <option value="CARC">Habitacao</option>
-                   <option value="CARC">Consumo</option>
-               </select>
-            </div>
-    </div>
-
-    <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Valor</label>
-        <div class="col-sm-10">
-            <input name="amount" type="text" class="form-control" placeholder="Valor do Financimento" required>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Periodo de Vigencia</label>
-        <div class="col-sm-10">
-            <input name="amount" type="text" class="form-control" placeholder="Periodo de Vigencia" required>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Taxa de Juros</label>
-        <div class="col-sm-10">
-            <input name="amount" type="text" class="form-control" placeholder="Taxa de Juro" required>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Rendimento Mensal</label>
-        <div class="col-sm-10">
-            <input name="amount" type="text" class="form-control" placeholder="Rendimento Mensal" required>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Taxa de Desconto Mensal</label>
-        <div class="col-sm-10">
-            <input name="amount" type="text" class="form-control" placeholder="Taxa de Desconto Mensal" required>
-        </div>
-    </div>
-		
-
-    <div class="form-group">
-        <label for="" class="col-sm-2 control-label" style="color:#009900">Payment Date</label>
-			<div class="col-sm-10">
-             	<div class="input-group date">
-                 	<div class="input-group-addon">
-                    	<i class="fa fa-calendar"></i>
-                  	</div>
-                  		<input type="text" class="form-control pull-right" id="datepicker" name="pay_date">
+            <label for="" class="col-sm-2 control-label" style="color:#009900">Tipo de Credito</label>
+                <div class="col-sm-10">
+                <select name="credittype" class="form-control" required>
+                    <option value="CARC">CARC</option>
+                    <option value="CARC">Habitacao</option>
+                    <option value="CARC">Consumo</option>
+                </select>
                 </div>
-            </div>
-	</div>
+        </div>
 
-</div>
+        <div class="form-group" >
+            <label for="" class="col-sm-2 control-label" style="color:#009900">Valor</label>
+            <div class="col-sm-10">
+                <input name="amount" type="number" class="form-control" placeholder="Valor do Financimento" name="lamout" required>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="" class="col-sm-2 control-label" style="color:#009900">Periodo Incial</label>
+            <div class="col-sm-10">
+                <input name="amount" type="date" class="form-control" placeholder="Periodo de Vigencia" name="pinit" required>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label for="" class="col-sm-2 control-label" style="color:#009900">Periodo Inicial</label>
+            <div class="col-sm-10">
+                <input name="amount" type="date" class="form-control" placeholder="Periodo de Vigencia" name="pfinal" required>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="" class="col-sm-2 control-label" style="color:#009900">Taxa de Juros</label>
+            <div class="col-sm-10">
+                <input name="amount" type="text" class="form-control" placeholder="Taxa de Juro" name="irate" required>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="" class="col-sm-2 control-label" style="color:#009900">Taxa de Desconto</label>
+            <div class="col-sm-10">
+                <input name="amount" type="text" class="form-control" placeholder="Rendimento Mensal" required>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" id="save" class="btn btn-primary btn-flat" name="simular"><i class="fa fa-times">&nbsp;Simular</i></button>
+        </div>
+</div>  
+
+<script>
+    $("#lamout").change(function()
+        {
+            calloan();
+        }
+
+    );
+
+    $("#irate").change(function()
+        {
+            calloan();
+        }
+
+    );
+
+    $("#pinit").change(function()
+        {
+            calloan();
+        }
+
+    );
+
+    function callon()
+    {
+        if($("#lamout").val() == "")
+        {
+            return false;
+        }
+
+        else if($("#pinit").val() == "")
+        {
+            return false;
+        }
+
+        else if($("#irate").val() == "")
+        {
+            return false;
+        }
+
+    else{
+        var totalamount = 0;
+        totalamount = (Number($("#lamount").val())* (Number($("#irate").val())/100) * (Number($("#lamount").val())
+
+    }
+    }
+
+</script>
+
